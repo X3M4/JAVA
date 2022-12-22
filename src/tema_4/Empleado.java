06b10c6109ab61b9027b98263a81a7f769347008
Chema Fernández <jofermo5@gmail.com>
2022-12-22 15:53:50 +0100

    Exercises with classes and methods

--- src/tema_4/Empleado.java
+++ src/tema_4/Empleado.java
@@ -1,13 +1,18 @@
 
 /*In this exercise we have this code:
  * // File Empleado.java
-In this branch I'll modify last exercise with a static method for returning the variable nombre
+In this branch I'll modify last exercise with a static method for returning the variable nombre. However, I'll need
+to modify Empleado method and to make nombre variable as obligated in the constructor.
  */
 package tema_4;
 
+import java.util.Scanner;
 public class Empleado {
+    Scanner entrada = new Scanner(System.in);
     static String nombre;
     int edad;
+    private static double sueldo;
+    private static String password = "password";
 
     public Empleado(String nombre, int edad){
         this.nombre = nombre;
@@ -17,5 +22,22 @@ public class Empleado {
     public static String getNombre(){
         return nombre;
     }
-    
+
+    public void setSueldo(double sueldo){
+        Empleado.sueldo = sueldo;
+    }
+
+    public double getSueldo() throws IllegalArgumentException{
+        System.out.println("Escriba el password del empleado");
+        String outPassword = entrada.next();
+        
+        if(!password.equals(outPassword)){
+            throw new IllegalArgumentException ("PASSWORD INCORRECTO");
+        }
+        else{
+            return sueldo;
+        }
+        
+    }
+
 }
--- src/tema_4/PruebaEmpleados.java
+++ src/tema_4/PruebaEmpleados.java
@@ -4,6 +4,13 @@ public class PruebaEmpleados {
     public static void main(String[] args){
         Empleado e1 = new Empleado("Pepe",34);
         System.out.println("Nombre: " + e1.getNombre());
+        e1.setSueldo(15000);
+        e1.setPassword("password");
         
+        try{
+            System.out.printf("Sueldo = %1.2f\n", e1.getSueldo());
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 }
