f077aaca9286bb63792f468d8d76bb1e0346d084
Chema Fernández <jofermo5@gmail.com>
2022-12-22 18:02:46 +0100

    Exercise Empleado.java modified

---
+++ src/tema_4/Punto2D.java
@@ -0,0 +1,22 @@
+package tema_4;
+
+public class Punto2D {
+    
+    static double x = 0, y = 0;
+
+   
+
+    public Punto2D(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX(){
+        return x;
+    }
+
+    public double getY(){
+        return y;
+    }
+}
+
---
+++ src/tema_4/pruebaPunto2D.java
@@ -0,0 +1,10 @@
+package tema_4;
+
+public class pruebaPunto2D {
+    public static void main(String[] args){
+        Punto2D p1 = new Punto2D(3.0, 2.5);
+        System.out.printf("%f - %f",p1.getX(), p1.getY());
+
+    }
+    
+}
