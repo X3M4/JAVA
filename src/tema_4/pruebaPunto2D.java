e353f12db5469346256981f640be34f9f58b470b
Chema Fernández <jofermo5@gmail.com>
2022-12-28 09:03:51 +0100

    nuevos cambios

--- src/tema_4/pruebaPunto2D.java
+++ src/tema_4/pruebaPunto2D.java
@@ -3,8 +3,9 @@ package tema_4;
 public class pruebaPunto2D {
     public static void main(String[] args){
         Punto2D p1 = new Punto2D(3.0, 2.5);
+        System.out.printf("%f - %f",p1.getX(), p1.getY());
         Punto2D p2 = new Punto2D();
-        Punto2D p3 = new Punto2D(5.2, -3.5);
+        Punto2D p3 = new Punto2D(5.2, 3.5);
         Punto2D p4 = new Punto2D();
 
         System.out.printf("%f - %f\n", p1.getX(), p1.getY());
@@ -12,10 +13,8 @@ public class pruebaPunto2D {
         System.out.printf("%f - %f\n", p3.getX(), p3.getY());
         System.out.printf("%f - %f\n", p4.getX(), p4.getY());
 
-        Punto2D p5 = p1.suma(p3);
-        System.out.printf("%f - %f\n", p5.getX(), p5.getY());
-        System.out.println(p1 + " + " + p3 + " = " + p5 );
-        System.out.println(p4 + " es igual a " + p2 + "?" + p4.equals(p2));
+        
+        
 
 
     }
